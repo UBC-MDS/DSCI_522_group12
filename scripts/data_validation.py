bbdadb041e3fd580cb20e293e8c4f1253c3f20bb
cs-uche <118601888+cs-uche@users.noreply.github.com>
2024-12-04 16:33:06 -0800

    created data vaidation script

---
+++ scripts/data_validation.py
@@ -0,0 +1,97 @@
+import click
+import os
+import pandas as pd
+import pandera as pa
+from sklearn.model_selection import train_test_split
+
+
+def clean_column_names(df):
+    """
+    Clean column names by converting them to lowercase, replacing spaces and dashes with underscores. 
+    
+    Parameters:
+    df (pd.DataFrame): The input pandas DataFrame whose column names need to be cleaned.
+
+    Returns:
+    pd.DataFrame: A DataFrame with cleaned column names.
+    """
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace(r'\s+', '_', regex=True)
+    df.columns = df.columns.str.replace(r'-', '_', regex=True)
+    df = df.rename({"departure/arrival_time_convenient":"time_convenient"}, axis=1)
+    
+    return df
+
+def main(raw_train_path, raw_test_path, val_train_path, val_test_path):
+    train_data = pd.read_csv(raw_train_path, index_col='Unnamed: 0')
+    test_data = pd.read_csv(raw_test_path, index_col='Unnamed: 0')
+
+    combined_dataset = pd.concat([train_data, test_data], axis=0).reset_index(drop=True)
+    assert len(combined_dataset) == (len(train_data) + len(test_data)), \
+        "Combined dataset length should equal the length of the train and test datasets"
+    combined_dataset = clean_column_names(combined_dataset)
+
+    # Data Validation
+    print(">>"*30)
+    missing_data_threshold = 0.05
+    schema = pa.DataFrameSchema(
+        {
+            "gender": pa.Column(str, pa.Check.isin(["Male", "Female"]), nullable=False),
+            "customer_type": pa.Column(str, pa.Check.isin(["Loyal Customer", "Disloyal Customer"]), nullable=False),
+            "age": pa.Column(int, pa.Check.between(0, 100), nullable=False),
+            "type_of_travel": pa.Column(str, pa.Check.isin(["Business travel", "Personal Travel"]), nullable=False),
+            "class": pa.Column(str, pa.Check.isin(["Eco", "Eco Plus", "Business"]), nullable=False),
+            "flight_distance": pa.Column(int, pa.Check.greater_than(0), nullable=False),
+            "inflight_wifi_service": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "time_convenient": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "ease_of_online_booking": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "gate_location": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "food_and_drink": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "online_boarding": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "seat_comfort": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "inflight_entertainment": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "on_board_service": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "leg_room_service": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "baggage_handling": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "checkin_service": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "inflight_service": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "cleanliness": pa.Column(int, pa.Check.between(0, 5), nullable=True),
+            "departure_delay_in_minutes": pa.Column(int, pa.Check.greater_than_or_equal_to(0), nullable=True),
+            "arrival_delay_in_minutes": pa.Column(float, pa.Check.greater_than_or_equal_to(0), nullable=True),
+            "satisfaction": pa.Column(str, pa.Check.isin(["neutral or dissatisfied", "satisfied"]), nullable=False),
+        },
+        checks=[
+            pa.Check(lambda df: ~(df.isna().all(axis=1)).any(), error="Empty rows found."),
+            pa.Check(lambda df: (df.isna().sum() / len(df) < missing_data_threshold).all(), error=f"Some columns have more than {missing_data_threshold*100}% missing values.")
+        ])
+
+    combined_dataset["customer_type"] = combined_dataset["customer_type"].str.title()
+    try:
+        schema.validate(combined_dataset, lazy=True)
+        print("Data validation passed.")
+    except pa.errors.SchemaErrors as e:
+        print(e.failure_cases)
+
+    # check for duplicates
+    all_ids = combined_dataset.id
+    if (len(set(all_ids)) == len(all_ids)):
+        print("No duplicates found")
+    else:
+        print("There are duplicate observations")
+    print(f"Number of duplicated observations = {combined_dataset.drop('id', axis=1).duplicated().sum()}")
+    print("<<"*30)
+
+    # splitting
+    test_size = 0.2
+    random_state = 123
+    train_data, test_data = train_test_split(combined_dataset, test_size = test_size, random_state = random_state)
+    train_data.to_csv(os.path.join(val_train_path, 'train.csv'), index=False)
+    test_data.to_csv(os.path.join(val_test_path, 'test.csv'), index=False)
+    print(">>"*30)
+    print("Validated and re-split data written out")
+    print("Train path: ", val_train_path)
+    print("Test path: ", val_test_path)
+    print("<<"*30)
+
+if __name__ == "__main__":
+    main()
