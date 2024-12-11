ca4b5f076179f0354119754ceda8b8d0c29aba9c
AzinPiran <az.p.ctc@gmail.com>
2024-12-11 13:39:28 -0800

    src and tests folder added, download_read_combine_data.py added, data_download.py modified

--- scripts/data_download.py
+++ scripts/data_download.py
@@ -1,28 +1,10 @@
-import pandas as pd
 import click
-import kagglehub
-from pathlib import Path
-
-def download_read_combine_data(kaggle_url):
-    # Try to download the dataset
-    try:
-        data_saved_path = Path(kagglehub.dataset_download(kaggle_url))
-    except ValueError:
-        print("The dataset link is invalid, please use kaggle's link 'teejmahal20/airline-passenger-satisfaction'")
-    except:
-        print("An unknown error occurred. Please try to provide 'teejmahal20/airline-passenger-satisfaction' as the link.")
-    
-    # Get the paths of the initial train and test sets
-    train_data_path = data_saved_path / "train.csv"
-    test_data_path = data_saved_path / "test.csv" 
-
-    # Read the datasets
-    train_data = pd.read_csv(train_data_path, index_col = "Unnamed: 0")
-    test_data = pd.read_csv(test_data_path, index_col = "Unnamed: 0")
-
-    # Combine the dataset and return it
-
-    return pd.concat([train_data, test_data], axis=0).reset_index(drop=True)
+import os
+import zipfile
+import requests
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.download_read_combine_data import download_read_combine_data
 
 
 @click.command()
@@ -40,37 +22,9 @@ def download_read_combine_data(kaggle_url):
               help="Do you want to overwrite the file if it exists?", 
               default=False)
 def main(url, save_to, file_to, force_save):
-    # Convert the string to Path
-    save_to = Path(save_to)
+    """Downloads the data from the web to a local filepath and combine it."""
+    download_read_combine_data(url, save_to, file_to, force_save)
 
-    # Download, read, combine and get the data
-    dataset = download_read_combine_data(url)
-
-    # If the path doesn't exist, create it
-    if not save_to.exists():
-        save_to.mkdir(parents=True, exist_ok=True)
-
-    # The path to save the dataset
-    file_to_save = save_to / file_to
-
-    # If the file exists and force save is False do nothing
-    if file_to_save.exists() and not force_save:
-        print(f"""
-                The file "{file_to_save}" already exists... 
-                The script will not overwrite the file. 
-                If you want to force save the file, specify argument --force_save=True
-                Terminating the script...
-        """)
-        return
-
-
-    # If save_to is a directory, not a file save the dataset, if not, raise an error
-    if save_to.is_dir():
-        dataset.to_csv(file_to_save, index=False)
-    else:
-        raise ValueError("The argument save_to is not a directory!")
-    
-    print(f"The dataset was successfully downloaded and saved in the directory: \033[1m{save_to}\033[0m\n")
 
 if __name__ == '__main__':
     main()
---
+++ src/download_read_combine_data.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import kagglehub
+from pathlib import Path
+
+def download_read_combine_data(url, save_to, file_to, force_save=False):
+    # Try to download the dataset
+    try:
+        data_saved_path = Path(kagglehub.dataset_download(url))
+    except ValueError:
+        print("The dataset link is invalid, please use kaggle's link 'teejmahal20/airline-passenger-satisfaction'")
+    except:
+        print("An unknown error occurred. Please try to provide 'teejmahal20/airline-passenger-satisfaction' as the link.")
+    
+    # Get the paths of the initial train and test sets
+    train_data_path = data_saved_path / "train.csv"
+    test_data_path = data_saved_path / "test.csv" 
+
+    # Read the datasets
+    train_data = pd.read_csv(train_data_path, index_col="Unnamed: 0")
+    test_data = pd.read_csv(test_data_path, index_col="Unnamed: 0")
+
+    # Combine the dataset
+    dataset = pd.concat([train_data, test_data], axis=0).reset_index(drop=True)
+    
+    # Convert the string to Path
+    save_to = Path(save_to)
+    
+    # If the path doesn't exist, create it
+    if not save_to.exists():
+        save_to.mkdir(parents=True, exist_ok=True)
+
+    # Save the combined dataset
+    combined_file_path = save_to / file_to
+    if combined_file_path.exists() and not force_save:
+        print(f"""
+                The file "{combined_file_path}" already exists... 
+                The script will not overwrite the file. 
+                If you want to force save the file, specify argument --force_save=True
+                Terminating the script...
+        """)
+        return
+
+    if save_to.is_dir():
+        dataset.to_csv(combined_file_path, index=False)
+    else:
+        raise ValueError("The argument save_to is not a directory!")
+
+   
+    # Create a 'raw' directory inside the 'save_to' folder if it doesn't exist
+    raw_folder = save_to / "raw"
+    if not raw_folder.exists():
+        raw_folder.mkdir(parents=True, exist_ok=True)
+
+    # Save the train and test datasets separately in the 'raw' folder
+    raw_train_file_path = raw_folder / "satisfaction_train.csv"
+    raw_test_file_path = raw_folder / "satisfaction_test.csv"
+
+    if not raw_train_file_path.exists() or force_save:
+        train_data.to_csv(raw_train_file_path, index=False)
+        print(f"Raw train dataset saved at: {raw_train_file_path}")
+
+    if not raw_test_file_path.exists() or force_save:
+        test_data.to_csv(raw_test_file_path, index=False)
+        print(f"Raw test dataset saved at: {raw_test_file_path}")
+
+    print(f"The combined dataset was successfully saved in the directory: \033[1m{save_to}\033[0m\n")
